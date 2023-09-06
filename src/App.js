478826d9e27707d8265630fc4dc402cce7d768ae
Atishay Jain <Atigit15@users.noreply.github.com>
2023-09-06 23:40:17 +0530

    idk

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+/* use react states and hooks
+use external API
+LordIcon Integration
+https://www.youtube.com/watch?v=b9eMGE7QtTk
+KEY : 66aca767
+async -> takes some time to fetch movies
+*/
+
 import './App.css';
+import MovieCard from './MovieCard';
+import SearchIcon from './search.svg'
+import { useEffect, useState } from 'react';
+import lottie from 'lottie-web';
+import { defineElement } from 'lord-icon-element';
+
+// define "lord-icon" custom element with default properties
+defineElement(lottie.loadAnimation)
+
+const API_URL = "https:/www.omdbapi.com?apikey=66aca767";
 
 function App() {
+  const [movies,setMovies] = useState([]);
+  const [searchTerm,setSearchTerm] = useState("");
+
+  const serachMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search)
+  }
+  useEffect(() => {
+    serachMovies('friends')
+  },[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <lord-icon trigger="loop" src="https://cdn.lordicon.com/xhitaejr.json" style={{height : '100px',width: '100px'}}></lord-icon>
+      <h1>MovieMania</h1>
+      <div className='search'>
+        <input
+          placeholder='Search for movies or tv shows'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}></input>
+          <img src={SearchIcon} alt='search' onClick={() => serachMovies(searchTerm)}></img>
+      </div>
+      {
+        movies.length > 0 ? 
+        (
+          <div className='container'>
+            {movies.map((movie) => (
+              <MovieCard movie={movie}></MovieCard>
+            ))}
+          </div>
+        ) : 
+        (
+          <div className='empty'>
+            <h2>No movies found</h2>
+          </div>
+        )
+      } 
     </div>
   );
 }
---
+++ src/MovieCard.jsx
@@ -0,0 +1,22 @@
+//any diifernec between .js and .jsx file
+
+import React from 'react'
+
+const MovieCard = ({movie}) => {
+  return (
+    <div className='movie'>
+        <div>
+            {movie.Year}
+        </div>
+        <div>
+            <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.title}></img>
+        </div>
+        <div>
+            <span>{movie.Type}</span>
+            <h3>{movie.Title}</h3>
+        </div>
+    </div>
+  )
+}
+
+export default MovieCard;
